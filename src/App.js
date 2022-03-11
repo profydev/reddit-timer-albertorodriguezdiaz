4396fa67201fe036c8b5f6d79118b3d88a1805d3
albertorodriguezdiaz <alberto@efectovirtual.com>
2022-03-11 14:35:47 -0500

    APP skeleton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-dom';
+import Search from './components/search/Search';
+import Index from './components/home/index';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Index} />
+        <Route exact path="/search" component={Search} />
+      </Switch>
+    </Router>
   );
 }
 
